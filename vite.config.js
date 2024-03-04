7be320c1b088f919076fa6aa40bf3952ca6af2dd
Jun <xjunsantos@gmail.com>
2024-03-04 09:26:03 +0800

    upload files

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import vue from "@vitejs/plugin-vue";
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        vue(),
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    build: {
+
+        /** If you set esmExternals to true, this plugins assumes that 
+          all external dependencies are ES modules */
+     
+        commonjsOptions: {
+           esmExternals: true 
+        },
+     }
+});
